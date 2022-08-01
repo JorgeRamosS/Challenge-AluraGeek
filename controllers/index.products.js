59404b96edf74813bec8a998f453bd60de3a5385
Jorge Ramos <99624026+JorgeRamosS@users.noreply.github.com>
2022-08-01 20:43:55 -0300

    Add files via upload

---
+++ controllers/index.products.js
@@ -0,0 +1,44 @@
+import { clientServices } from "../app/client-services.js";
+
+const nuevoProducto = (url, nombre, precio, id) => {
+  const linea = document.createElement("a");
+  linea.classList.add("link");
+  const contenido = `
+      <div class="item">
+        <img src="${url}" alt="item1" class="imagen"/>
+        <div class="descripcion">
+          <p>${nombre}</p>
+          <p class="precio">${precio}</p>
+          <a href="./detalleprod.html?id=${id}" class="link__color">
+            Ver Producto
+          </a>
+        </div>
+      </div>
+    `;
+  linea.innerHTML = contenido;
+  return linea;
+};
+
+const div = document.querySelector("[data-sw]");
+const divb = document.querySelector("[data-consolas]");
+const divc = document.querySelector("[data-diversos]");
+
+clientServices
+  .listaItems()
+  .then((data) => {
+    data.forEach(({ url, nombre, categoria, precio, id }) => {
+      if (categoria == "Star Wars") {
+        const nuevoItem = nuevoProducto(url, nombre, precio, id);
+        div.appendChild(nuevoItem);
+      }
+      if (categoria == "Consolas") {
+        const nuevoItem = nuevoProducto(url, nombre, precio, id);
+        divb.appendChild(nuevoItem);
+      }
+      if (categoria == "Diversos") {
+        const nuevoItem = nuevoProducto(url, nombre, precio, id);
+        divc.appendChild(nuevoItem);
+      }
+    });
+  })
+  .catch((error) => alert("Ocurrio un error"));
