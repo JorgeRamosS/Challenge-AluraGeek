545c05aa14518abe39ac6ee10a8660841364cf18
Jorge Ramos <99624026+JorgeRamosS@users.noreply.github.com>
2022-08-02 22:58:44 -0300

    Delete app directory

--- app/icono.buscar.js
+++
@@ -1,7 +0,0 @@
-const bx = document.querySelector(".bx");
-bx.addEventListener("click", () => {
-  const buscar = document.querySelector(".buscarEstilo");
-  const logo = document.querySelector(".logo");
-  logo.classList.toggle("ocultar");
-  buscar.classList.toggle("mostrar");
-});
